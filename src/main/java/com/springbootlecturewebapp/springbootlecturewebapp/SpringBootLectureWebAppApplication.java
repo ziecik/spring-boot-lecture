22195681097ba0a666b93e41eef4a37a0f330969
Jaroslaw Ziecik <jaroslaw.ziecik@gmail.com>
2020-09-08 18:39:28 +0200

    Initialize spring boot project with basic dependencies: Spring Web & Spring Data JPA

---
+++ src/main/java/com/springbootlecturewebapp/springbootlecturewebapp/SpringBootLectureWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.springbootlecturewebapp.springbootlecturewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLectureWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLectureWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootlecturewebapp/springbootlecturewebapp/SpringBootLectureWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootlecturewebapp.springbootlecturewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLectureWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
